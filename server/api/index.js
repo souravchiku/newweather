9361bef04d5c3ece33ba9cd9c15b6d6a1bd614cb
sourav <souravchiku50@gmail.com>
2021-04-23 19:01:28 +0530

    update

--- server/api/index.js
+++ server/api/index.js
@@ -6,16 +6,16 @@ const router = express.Router();
 const Weather = require("./weather");
 
 // GET Request - statically get the weather data from the weather api
-router.get("/weather",  async (req, res) => {
-    let weather = new Weather();
+// router.get("/weather",  async (req, res) => {
+//     let weather = new Weather();
 
-    // Fixing the params of zipcode and tempMetric for an example GET request
-    let weatherData = await weather.getWeatherData(98052, "us");
+//     // Fixing the params of zipcode and tempMetric for an example GET request
+//     let weatherData = await weather.getWeatherData(98052, "us");
 
-    // Content that will be sent will be a prettified json
-    res.header("Content-Type",'application/json');
-    res.send(JSON.stringify(weatherData, null, 4));
-});
+//     // Content that will be sent will be a prettified json
+//     res.header("Content-Type",'application/json');
+//     res.send(JSON.stringify(weatherData, null, 4));
+// });
 
 // POST Request - dynamically get the weather data based on request body
 router.post("/weather",  async (req, res) => {
